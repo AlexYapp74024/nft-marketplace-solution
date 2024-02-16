a46d7dd41045bc3eb84d49e0853e6a3904bc5210
Alex Yapp <alexy74024@gmail.com>
2024-02-16 13:00:41 +0800

    a

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-ganache-cli --host "172.31.9.138"  --port 8545 --networkId 5777 -d 
-# npm run start
+cd ~/nft-marketplace-solution
+npm run start
+#/home/ubuntu/.nvm/versions/node/v16.16.0/bin/ganache -v
+#ganache-cli --host "172.31.9.138"  --port 8545 --networkId 5777 -d
